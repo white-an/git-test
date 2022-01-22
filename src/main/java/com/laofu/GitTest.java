ee053ccbb6615189e9fa5b2588028bffb09851ca
white-an <98173968+white-an@users.noreply.github.com>
2022-01-22 13:47:23 +0800

    Update GitTest.java

--- src/main/java/com/laofu/GitTest.java
+++ src/main/java/com/laofu/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("github push test");
+        System.out.println("github pull test");
     }
 }
