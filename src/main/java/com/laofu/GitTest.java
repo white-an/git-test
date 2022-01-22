c781c6551dbc89122c788f99a9d162622a7e0275
an <317904537@qq.com>
2022-01-22 12:01:22 +0800

    hot fix test

--- src/main/java/com/laofu/GitTest.java
+++ src/main/java/com/laofu/GitTest.java
@@ -3,5 +3,6 @@ package com.laofu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hot-fix test");
     }
 }
